801a37d57429c6d31614af8d6c68f41cdd6cba60
ConservadoMarivic <2015081245@ust-ics.mygbiz.com>
2018-12-12 23:49:02 +0800

    version 3 preview pages final

---
+++ app/src/main/java/marivic/conservado/com/projectlayout/Laloma.java
@@ -0,0 +1,13 @@
+package marivic.conservado.com.projectlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Laloma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laloma);
+    }
+}
---
+++ app/src/main/java/marivic/conservado/com/projectlayout/ManilaCentralUniversity.java
@@ -0,0 +1,13 @@
+package marivic.conservado.com.projectlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManilaCentralUniversity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manila_central_university);
+    }
+}
